559f587ff3eca8de8648c41cd0d5d04942b36056
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-08-17 11:13:42 +0200

    Reorganize project structure and create movie model

--- app/src/main/java/com/sipox11/retrofitgsonmoviesapiclientdemo/ui/MainActivity.java
+++ app/src/main/java/com/sipox11/retrofitgsonmoviesapiclientdemo/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.sipox11.retrofitgsonmoviesapiclientdemo;
+package com.sipox11.retrofitgsonmoviesapiclientdemo.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.sipox11.retrofitgsonmoviesapiclientdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
